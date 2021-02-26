0e2f9e28a4942825c1237c75b866cb5f85eb1b34
Soham Sawant <geekysoham@gmail.com>
2021-02-26 20:33:51 +0530

    created themes.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:dog_app/pages/home_page.dart';
+import 'package:dog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    themeMode: ThemeMode.light,
+    theme: MyTheme.lightTheme(context),
+    darkTheme: MyTheme.darkTheme(context),
+    
     routes: {
       '/': (context) => HomePage(),
     },
-  ));
+  );
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,19 @@
+import 'package:dog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(),
-     backgroundColor: Colors.white, 
+      backgroundColor: MyTheme.creamColor,
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ),
     );
   }
 }

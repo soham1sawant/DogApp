9b5092ba846ea9f7273701f0aa3b05a41a6921fc
Soham Sawant <geekysoham@gmail.com>
2021-02-26 20:07:32 +0530

    created initial application

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
+import 'package:dog_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+    },
+  ));
 }

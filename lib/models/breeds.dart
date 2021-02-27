64ea8611aab109efeba04f6f21b7aecd93fc79c2
Soham Sawant <geekysoham@gmail.com>
2021-02-27 21:39:37 +0530

    started with json parsing

---
+++ lib/models/breeds.dart
@@ -0,0 +1,29 @@
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromMap(jsonString);
+
+import 'dart:convert';
+
+Welcome welcomeFromMap(String str) => Welcome.fromMap(json.decode(str));
+
+String welcomeToMap(Welcome data) => json.encode(data.toMap());
+
+class Welcome {
+    Welcome({
+        this.message,
+        this.status,
+    });
+
+    Map<String, List<String>> message;
+    String status;
+
+    factory Welcome.fromMap(Map<String, dynamic> json) => Welcome(
+        message: Map.from(json["message"]).map((k, v) => MapEntry<String, List<String>>(k, List<String>.from(v.map((x) => x)))),
+        status: json["status"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "message": Map.from(message).map((k, v) => MapEntry<String, dynamic>(k, List<dynamic>.from(v.map((x) => x)))),
+        "status": status,
+    };
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,33 @@
+import 'dart:convert';
+import 'package:dog_app/models/breeds.dart';
 import 'package:dog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    Response jsonString = await get("https://dog.ceo/api/breeds/list/all");
+    final data = welcomeFromMap(jsonString.body);
+    print(data);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyTheme.creamColor,
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.all(32),
+          padding: EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [

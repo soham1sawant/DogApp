bebacda8c6ed6fc8be1a0697b4d00021f755b5ad
Soham Sawant <geekysoham@gmail.com>
2021-03-06 21:37:00 +0530

    started with json parsing

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,33 +1,14 @@
-import 'dart:convert';
-import 'package:dog_app/models/breeds.dart';
 import 'package:dog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-    loadData();
-  }
-
-  loadData() async {
-    Response jsonString = await get("https://dog.ceo/api/breeds/list/all");
-    final data = welcomeFromMap(jsonString.body);
-    print(data);
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyTheme.creamColor,
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.all(32),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [

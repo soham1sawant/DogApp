cc096c75cefeff3b2d714ffdffbec10629f8f0b3
Soham Sawant <geekysoham@gmail.com>
2022-02-13 10:39:03 +0530

    added badge to favourite icon

--- lib/presentation/widgets/main_header.dart
+++ lib/presentation/widgets/main_header.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/favourites/favourites_bloc.dart';
 
 class MainHeader extends StatelessWidget {
   final header;
@@ -26,13 +30,31 @@ class MainHeader extends StatelessWidget {
               : size.height * 0.007,
         ),
         if (icon)
-          IconButton(
-            icon: const Icon(Icons.favorite),
-            color: Theme.of(context).primaryColor,
-            iconSize: orientation == Orientation.portrait
-                ? size.height * 0.05
-                : size.height * 0.075,
-            onPressed: () => Navigator.of(context).pushNamed("/favourites"),
+          BlocBuilder<FavouritesBloc, FavouritesState>(
+            builder: (context, state) {
+              if (state is FavouritesLoaded) {
+                return Badge(
+                  badgeContent: Text(
+                    state.favouritesList.favourites.length.toString(),
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                  badgeColor: Theme.of(context).focusColor,
+                  position: BadgePosition.topEnd(top: 0, end: 0),
+                  animationType: BadgeAnimationType.scale,
+                  child: IconButton(
+                    icon: const Icon(Icons.favorite),
+                    color: Theme.of(context).primaryColor,
+                    iconSize: orientation == Orientation.portrait
+                        ? size.height * 0.05
+                        : size.height * 0.075,
+                    onPressed: () =>
+                        Navigator.of(context).pushNamed("/favourites"),
+                  ),
+                );
+              } else {
+                return const Center(child: Icon(Icons.error_outline));
+              }
+            },
           ),
       ],
     );

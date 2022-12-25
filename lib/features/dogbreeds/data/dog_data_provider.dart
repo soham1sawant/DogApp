b7f09e6232f09941ae76c8130417307a1ad3c6c3
Soham Sawant <geekysoham@gmail.com>
2022-12-25 12:58:59 +0530

    added correct api key

--- lib/features/dogbreeds/data/dog_data_provider.dart
+++ lib/features/dogbreeds/data/dog_data_provider.dart
@@ -4,11 +4,11 @@ import 'package:http/http.dart' as http;
 import '../../../core/auth/secrets.dart';
 
 class DogDataProvider {
-
   Future<http.Response> getRawDogData() async {
     http.Response response = await http.get(
-        Uri.parse("https://api.thedogapi.com/v1/breeds"),
-        headers: {HttpHeaders.authorizationHeader: '$API_KEY'});
+      Uri.parse("https://api.thedogapi.com/v1/breeds"),
+      headers: {HttpHeaders.authorizationHeader: myApiKey},
+    );
 
     return response;
   }
